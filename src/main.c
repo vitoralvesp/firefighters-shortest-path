d1dd0c52bac35d684d90fa8179e04be3f94ad6be
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-11-04 21:09:44 -0300

    reading characters from file done

--- src/main.c
+++ src/main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void remove_in_str(char str[6], char character) {
-    int k = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != character) {
-            str[k] = str[i];
-            k++;
+void build_mtrx(int mtrx[100][3], char buffer[100], int k) {
+    int col = 0;
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        if (buffer[i] >= 48 && buffer[i] <= 57) {
+            mtrx[k][col] = buffer[i] - '0';
+            col++;
         }
     }
-    str[k] = '\0';
-}
-
-
-void build_mtrx(int mtrx[100][3], char buffer[6], int k) {
-    int value;
-    remove_in_str(buffer, ' ');
-    remove_in_str(buffer, '\n');
-    for (int i = 0; buffer[i] != '\0'; i++) {
-        value = buffer[i] - '0';
-        if (value < 0) mtrx[k][i] = 0;
-        else mtrx[k][i] = value;
-    }
-    // printf("\n");
 }
 
 
@@ -36,16 +22,21 @@ int main() {
     } else {
 
         int mtrx[100][3];
-        char buffer[6];
-        int k;
+        char buffer[100];
+        int k = 0;
 
-        while (fgets(buffer, 6, file_ptr)) {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 3; j++)
+                mtrx[i][j] = 0;
+        }
+
+        while (fgets(buffer, 100, file_ptr)) {
             build_mtrx(mtrx, buffer, k);
             k++;
         }
 
-        printf("\nmtrx:\n");
-        for (int i = 0; i < 20; i++) {
+        printf("mtrx:\n");
+        for (int i = 0; i < k; i++) {
             for (int j = 0; j < 3; j++)
                 printf("%d ", mtrx[i][j]);
             printf("\n");
