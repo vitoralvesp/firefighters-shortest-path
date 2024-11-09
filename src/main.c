a08459ab74c4a4927eaab9879b54510719ee3750
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-11-09 15:56:38 -0300

    documentação

--- src/main.c
+++ src/main.c
@@ -6,6 +6,11 @@
 #define MAX_CORNERS (10)
 #define CHARS_TO_READ_IN_LINE (7) // 3 numeros, 2 espacos, 1 '\n'
 
+/* build_mtrx: recebe uma buffer com elementos numéricos do tipo char e os converte para 
+   inteiros em uma matriz utilizada para armazenar as esquinas e custos (void)
+   'mtrx': matriz do tipo int de tamanho 100 x 3
+   'buffer': vetor de elementos numéricos do tipo char com parte do conteúdo lido de um arquivo
+   'k': identificador da linha que o conteúdo do buffer será alocado na matriz  */
 void build_mtrx(int mtrx[MAX_LINES_INPUT][3], char buffer[MAX_LINES_INPUT], int k) {
     int col = 0;
     for (int i = 0; buffer[i] != '\0'; i++) {
@@ -16,59 +21,70 @@ void build_mtrx(int mtrx[MAX_LINES_INPUT][3], char buffer[MAX_LINES_INPUT], int 
     }
 }
 
-//build_table(matriz,tabela,quantidade de linhas) -> monta uma tabela, 
-//onde a distancia entre uma esquina 'x' e outra 'y' esta em 'tabela[x-1][y-1]'
+/* build_table: monta uma tabela apenas com os custos do percurso de uma esquina i até uma esquina j (void)
+   'mtrx':  matriz do tipo int de tamanho 100 x 3
+   'table': tabela do tipo int que será montada com os custos do percurso das esquinas em mtrx
+   'n_rows': total de linhas da matriz */
 void build_table(int mtrx[MAX_LINES_INPUT][3], int table[MAX_CORNERS][MAX_CORNERS], int n_rows){
-    for(int row = 0; row < n_rows; row++){
-        //A cada linha da matriz, armazena a distancia das esquinas na tabela
-        //printf("(%d,%d) = %d\n", mtrx[row][0], mtrx[row][1], mtrx[row][2]);
+    for(int row = 0; row < n_rows; row++)
         table[mtrx[row][0]-1][mtrx[row][1]-1] = mtrx[row][2]; 
-        //printf("m[%d][%d] = %d\n\n",mtrx[row][0]-1,mtrx[row][1]-1,table[mtrx[row][0]-1][mtrx[row][1]-1]);
-    }
-   
 }
-int get_min_idx (int v[],int used[], int size){
+
+/* get_min_idx: retorna o índice do menor elemento de um vetor de inteiros (int)
+   'v': vetor de inteiros que será analisado
+   'used': vetor que identifica os elementos que estão sendo utilizados no vetor
+   'size': tamanho do vetor 
+   'min': índice do menor elemento no vetor */
+int get_min_idx (int v[], int used[], int size){
     int min = 1;
-    for(int i = 2; i < size; i++){
-        if(v[i] < v[min] && !used[i]) {
+    for (int i = 2; i < size; i++) {
+        if (v[i] < v[min] && !used[i]) {
             min = i;
         }
     }
-   
     return min;
 }
 
+/* check: verifica se uma esquina já foi inserida no vetor de caminho das esquinas até a esquina de destino (int)
+   'check_number': elemento que será procurado no vetor
+   'path': vetor de inteiros que representa as esquinas percorridas
+   'path_size': tamanho do vetor de caminhos
+   'return 1': elemento encontrado
+   'return 0': elemento não encontrado */
 int check(int check_number, int path[], int path_size) {
     for (int i = 0; i < path_size; i++)
         if (path[i] == check_number) return 1;
     return 0;
 }
 
-
-//TODO: Montar vetor de tempo
-//TODO: Adicionar parametro de vetor que mostre onde ocorre qual a esquina de origem
-//TODO: Criar rota
-void generate_roadtrip(int t[], int or[],int r[], int corner_on_fire, int biggest_corner, int used[],int table[MAX_CORNERS][MAX_CORNERS]){
+/* generate_roadtrip: analisa as possibilidades armazenadas na tabela em relação aos custos dos percursos
+   das esquinas e retorna o percurso e tempo ótimos para uma esquina de destino x (void)
+   'optimal_time': vetor de tempos que conterá o tempo ótimo para a esquina de destino
+   'optimal_path': vetor de caminhos que conterá a trajetória ótima de todas as esquinas até a esquina de destino
+   'corner_on_fire': esquina de destino
+   'biggest_corner': 
+ */
+void generate_roadtrip(int optimal_time[], int optimal_path[], int corner_on_fire, int biggest_corner, int used[],int table[MAX_CORNERS][MAX_CORNERS]){
     int count_used = 1,corner = 1, originCost, destCost, routeCost, origin = corner_on_fire, path_idx = 0;
 
      //Inicializando com infinitos, exceto a esquina inicial
-    for(int i = 1; i < biggest_corner; i++) t[i] = INT_MAX;
+    for(int i = 1; i < biggest_corner; i++) optimal_time[i] = INT_MAX;
 
-    t[0] = 0;
+    optimal_time[0] = 0;
     used[0] = 1;     
 
     while(count_used < biggest_corner){ //equivalente: enquanto E nao estiver vazio 
-        originCost = t[corner-1];
+        originCost = optimal_time[corner-1];
         for(int corner_idx = 0; corner_idx<biggest_corner; corner_idx++){
             routeCost = table[corner-1][corner_idx];
-            destCost = t[corner_idx];
+            destCost = optimal_time[corner_idx];
             /*Ignorar se:
                 -Nao houver caminho (currTime = 0)
                 -destino = origem (corner_idx = corner)
                 -Ja foi vistada
             */
             if (!check(corner, r, biggest_corner)) {
-                r[path_idx] = corner;
+                optimal_path[path_idx] = corner;
                 path_idx++;
             }
             
@@ -76,25 +92,16 @@ void generate_roadtrip(int t[], int or[],int r[], int corner_on_fire, int bigges
             
             //Se descobre um tempo melhor, substitui
             if(originCost + routeCost < destCost) {
-                t[corner_idx] = originCost + routeCost;
+                optimal_time[corner_idx] = originCost + routeCost;
             }
         }
         //Mudando a esquina de referencia
         // printf("Esquina atual: %d\n", corner);
         // printf("Trocando esquina...\n");
-        corner = get_min_idx(t,used,biggest_corner) + 1;
+        corner = get_min_idx(optimal_time, used, biggest_corner) + 1;
         count_used++;
         used[corner-1] = 1;
     }
-
-    // while(origin != 1){
-    //     int corner_idx = 0, r_idx = 0;
-    //     while(corner_idx < origin-1) corner_idx++; //Procurar esquina
-    //     r[biggest_corner - r_idx++] = or[corner_idx]; //adicionar a rota a sua origem
-    //     origin = or[corner_idx]+1; //trocar a esquina a procurar
-    // }
-
-
 }
 
 
